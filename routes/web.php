5ef55504119283a09f9d4956b85b92ff719786bf
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2022-06-30 15:12:19 -0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -15,40 +15,31 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\EventController;
 
-use App\Http\Middleware\Authenticate;
 
+//Rota raiz, com o middleware que faz a autenticação
+Route::get('/', [EventController::class, 'index'])->middleware('auth');
 
-Route::middleware([Authenticate::class])->group(function () {
+//Rota que adiciona eventos ao DB
+Route::post('/', [EventController::class, 'store'])->name('store');
 
+//Rota que deleta o evento do DB, a partir do id do evento
+Route::delete('/{id}', [EventController::class, 'destroy'])->name('delete');
 
-    //Rota raiz, com o middleware que faz a autenticação
-        Route::get('/', [EventController::class, 'index']);
+//Rota que atualiza os dados do evento selecionado
+Route::put('/update/{id}', [EventController::class, 'update'])->name('atualizar');
 
-    //Rota que adiciona eventos ao DB
-        Route::post('/', [EventController::class, 'store'])->name('store');
+//Rota que busca os evetos que usuário logado criou
+Route::get('/{name}/myevents', [EventController::class, 'myEvents'])->name('myEvents');
 
-    //Rota que deleta o evento do DB, a partir do id do evento
-        Route::delete('/{id}', [EventController::class, 'destroy'])->name('delete');
+//Rota que confirma o usuário no evento clicado
+Route::post('/{id}', [EventController::class, 'confirm'])->name('confirmPresence');
 
-    //Rota que atualiza os dados do evento selecionado
-        Route::put('/update/{id}', [EventController::class, 'update'])->name('atualizar');
+//Rota que cancela o usuário no evento clicado
+Route::put('/cancel/{id}', [EventController::class, 'cancel'])->name('cancelPresence');
 
-    //Rota que busca os evetos que usuário logado criou
-        Route::get('/{name}/myevents', [EventController::class, 'myEvents'])->name('myEvents');
+//Rota que lista a partir do evento a lista de usuários que estão cadastrados
+Route::get('/list/{event}', [EventController::class, 'list'])->name('presenceList');
 
-    //Rota que confirma o usuário no evento clicado
-        Route::post('/{id}', [EventController::class, 'confirm'])->name('confirmPresence');
-
-    //Rota que cancela o usuário no evento clicado
-        Route::put('/cancel/{id}', [EventController::class, 'cancel'])->name('cancelPresence');
-
-    //Rota que lista a partir do evento a lista de usuários que estão cadastrados
-        Route::get('/list/{event}', [EventController::class, 'list'])->name('presenceList');
-
-    //Rota que mostra quais eventos o usuário logado confirmou presença
-        Route::get('/{name}/confirmeds', [EventController:: class, 'myConfirmedEvents'])->name('myConfirmedEvents');
-
-
-});
-
+//Rota que mostra quais eventos o usuário logado confirmou presença
+Route::get('/{name}/confirmeds', [EventController:: class, 'myConfirmedEvents'])->name('myConfirmedEvents');
 
